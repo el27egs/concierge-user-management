5fa45698995323d261ea18560ee97632086ffb25
el27egs <el27egs@gmail.com>
2022-05-19 14:03:54 -0500

    Adding relation between managements and locations & changind user_id data type

--- src/main/resources/db/changelog/data/data_managements.sql
+++ src/main/resources/db/changelog/data/data_managements.sql
@@ -1,2 +1,2 @@
-INSERT INTO managements (management_id, "name", user_id)
-VALUES(1, '501A-Enero-2021-Julio-2022', 1);
+INSERT INTO managements (management_id, "name", user_id, location_id)
+VALUES(1, '501A-Enero-2021-Julio-2022', '53a3c065-8c50-46ca-9594-3c82bd8c6e2a',1);
--- src/main/resources/db/changelog/data/data_users_locations.sql
+++ src/main/resources/db/changelog/data/data_users_locations.sql
@@ -1,1 +1,1 @@
-INSERT INTO users_locations (user_location_id, user_id, location_id) VALUES(1, 1, 1);
+INSERT INTO users_locations (user_location_id, user_id, location_id) VALUES(1, '53a3c065-8c50-46ca-9594-3c82bd8c6e2a', 1);
